fe57e919159ca858cae61df5c12c9448f92e37a8
bkontos <brianrkontos@gmail.com>
2024-01-16 18:24:44 -0700

    Started pawn class

---
+++ pawn.py
@@ -0,0 +1,13 @@
+import pygame
+
+class Pawn:
+    def __init__(self, color, position):
+        self.color = color 
+        self.position = position
+
+    def get_possible_moves(self, board):
+       # method to get possible moves
+       pass
+    def draw(self, screen):
+        # Code to draw the pawn on the screen
+        pass
